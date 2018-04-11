523ad01dd615a6857fec890c5dc5bc213579be02
marcohcanada <38083703+marcohcanada@users.noreply.github.com>
2018-04-11 15:37:29 -0400

    Update HomeController.cs

--- PayrollSystemV1.0 (Beta)/Controllers/HomeController.cs
+++ PayrollSystemV1.0 (Beta)/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using PayrollSystemV1_0__Beta.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PayrollSystemV1._0__Beta_.Controllers
+namespace PayrollSystemV1_0__Beta.Controllers
 {
     public class HomeController : Controller
     {
@@ -12,7 +13,20 @@ namespace PayrollSystemV1._0__Beta_.Controllers
         public ActionResult Index()
         {
             return View();
-            int x;
+        }
+        // GET: /Home/Show
+        public ActionResult Show()
+        {
+            return View("Show", new DB());
+        }
+
+        // GET: /Home/Set?first=x,last=y
+        public ActionResult Set(string first, string last)
+        {
+            DB.WriteDB(first, last);
+            ViewBag.First = first;
+            ViewBag.Last = last;
+            return View();
         }
     }
 }
