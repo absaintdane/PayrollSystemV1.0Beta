f562477d85113279029c211af2981b6ad4fcb93a
Hanna <John@LAPTOP-37ID69LP>
2018-04-11 16:09:00 -0400

    VehicleClass
    Added Vehicle class.

---
+++ PayrollSystemV1.0 (Beta)/Models/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayrollSystemV1._0__Beta_.Models
+{
+    public abstract class Vehicle
+    {
+    }
+}
